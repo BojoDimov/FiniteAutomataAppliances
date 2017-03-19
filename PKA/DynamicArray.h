9d8b7fb4ab77922c0f60947f5e19885ee561c089
MASTERRACE\Bozhidar <bojko958@abv.bg>
2017-03-19 22:54:59 +0200

    tarjan table

--- PKA/DynamicArray.h
+++ PKA/DynamicArray.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#define DA_INITIAL_CAP 256
 
 struct dynamic_array {
 	int size;
@@ -6,9 +7,17 @@ struct dynamic_array {
 	int* arr;
 
 	dynamic_array() {
-		capacity = 256;
+		capacity = DA_INITIAL_CAP;
 		size = 0;
 		arr = new int[capacity];
+	}
+
+	int operator[](int pos) {
+		if (pos >= size) {
+			throw "Index out of bounds";
+		}
+
+		return arr[pos];
 	}
 
 	~dynamic_array() {
@@ -19,6 +28,17 @@ struct dynamic_array {
 void resize(dynamic_array&);
 void insert(dynamic_array&, int);
 void init(dynamic_array&, int);
+
+void soft_reset(dynamic_array& da) {
+	da.size = 0;
+}
+
+void hard_reset(dynamic_array& da) {
+	da.size = 0;
+	da.capacity = DA_INITIAL_CAP;
+	delete[] da.arr;
+	da.arr = new int[DA_INITIAL_CAP];
+}
 
 void init(dynamic_array& arr, int x) {
 	for (int i = 0; i < arr.capacity; i++) {
@@ -36,7 +56,7 @@ void insert(dynamic_array& data, int x) {
 }
 
 void resize(dynamic_array& data) {
-	std::cout << "resizing array: " << data.arr << std::endl << data.capacity << " - old capacity\n";
+	//std::cout << "resizing array: " << data.arr << std::endl << data.capacity << " - old capacity\n";
 	int new_capacity = 2 * data.capacity;
 	int* new_arr = new int[new_capacity];
 	for (int i = 0; i < data.size; i++) {
